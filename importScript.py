da2bc027097fefb80198a572fd44a5b94d6a90c5
RJ Lock <19433651+lockrc@users.noreply.github.com>
2018-02-06 22:22:11 -0500

    Added database error handling.

--- importScript.py
+++ importScript.py
@@ -6,8 +6,23 @@
 
 import csv
 import mysql.connector
+from mysql.connector import errorcode
+import time
 
-cnx = mysql.connector.connect(user='root', password='reidb',
-    host='localhost',
-    database='rei')
+try:
+    cnx = mysql.connector.connect(user='root', password='reidb',
+        host='localhost',
+        database='test')
+
+except mysql.connector.Error as err:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Something is wrong with your user name or password")
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Database does not exist")
+  else:
+    print(err)
+
+print "Success!"
+time.sleep(10)
+
 cnx.close()
