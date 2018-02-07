9915a195a45bb9caaa6b38c63031b9d636b2bf91
RJ Lock <19433651+lockrc@users.noreply.github.com>
2018-02-07 16:21:51 -0500

    Corrected new database name and indentation

--- importScript.py
+++ importScript.py
@@ -15,14 +15,14 @@ try:
     reidb = mysql.connector.connect(user='root',
         password='reidb',
         host='localhost',
-        database='test')
+        database='rei')
 except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with your user name or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
-  else:
-    print(err)
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
 
 # Create database Cursor #
 cursor = reidb.cursor()
