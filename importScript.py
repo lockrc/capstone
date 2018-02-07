3238ddd8a829e4f4bcbd1df3c5ea0dbea295710e
RJ Lock <19433651+lockrc@users.noreply.github.com>
2018-02-06 22:13:40 -0500

    Fixed Header, Finally figured out private email, fixed mysql connection.

--- importScript.py
+++ importScript.py
@@ -1,4 +1,8 @@
-# Copyright (c) 2018 RJ Lock
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+#
+# Copyright (c) 2018 Richard Lock (lockrc@appstate.edu)
+# GNU GPLv3 <https://www.gnu.org/licenses/gpl-3.0.en.html>
 
 import csv
-import MySQLdb
+import mysql.connector
