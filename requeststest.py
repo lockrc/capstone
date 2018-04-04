79ceaffffbc03ae77969dbe0300ad88fa12c3b29
RJ Lock <19433651+lockrc@users.noreply.github.com>
2018-04-04 15:56:19 -0400

    Revert "Added tests used to learn functionality of requests and untangle"
    This reverts commit 165aac78e19d4c486649fdb0955995657bd923b4.

--- requeststest.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-r = requests.get('http://152.10.10.162/sm101.xml')
-print(r.text)
-
-r = requests.get('https://api.enphaseenergy.com/api/v2/systems/664018/summary?key=67551e09a7f4aa3609816466b9c4a757&user_id=4e6a4d344d7a45790a')
-print(r.text)
--- untangletest.py
+++
@@ -1,4 +0,0 @@
-import untangle
-
-obj = untangle.parse('http://152.10.10.162/sm101.xml')
-print obj.Maverick.NodeID.cdata
